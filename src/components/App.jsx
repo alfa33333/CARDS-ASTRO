ca409d3cf89f53e9503b6358f3a1fbd77c39c9f7
Antonio Herrera Martin <44150484+alfa33333@users.noreply.github.com>
2025-07-31 16:24:22 -0600

    add NavBar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import Card from './card.jsx';
+import NavBar from './NavBar.jsx';
 import style from "../css/app.module.css";
 
 function App() {
   return (
     <div className={style.App}>
-      <Card 
+      <NavBar />  
+      <Card
         picture="https://picsum.photos/400/300"
         foodName="KIBI"
        />
+
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+    <div style={{
+        borderBottom: '3px solid black',
+        height: '78px',
+        backgroundColor: '#6A3FFF',
+    }}>
+
+    </div>
+    );
+};
+
+export default NavBar;
