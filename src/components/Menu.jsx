d536a0215d7d6e2cca4d944a30c55b73928611c3
Antonio Herrera Martin <44150484+alfa33333@users.noreply.github.com>
2025-08-02 16:48:08 -0600

    feat: implement responsive navigation with hamburger menu and dropdown links

---
+++ src/components/Menu.jsx
@@ -0,0 +1,42 @@
+import styles from "./NavBar.module.css"; // Assuming a CSS file for styling
+
+function HamButtom({ toggleMenu }) {
+  return (
+    <div
+      className={`${styles["hamburger-menu"]} ${styles["small-screen"]}`}
+      onClick={toggleMenu}
+      style={{ marginLeft: "auto" }}
+    >
+      <div className={styles.bar}></div>
+      <div className={styles.bar}></div>
+      <div className={styles.bar}></div>
+    </div>
+  );
+}
+
+function MenuLinks(){
+    const linkNames = ["Food Library", "About"];
+
+    return (
+        <div className={`${styles['dropdown-menu']} ${styles['small-screen']}`}>
+            {linkNames.map((name, index) => (
+                <a key={index} href={`#link${index + 1}`}>{name}</a>
+            ))}
+        </div>
+    );
+}
+
+function Menu() {
+  return (
+    <div
+      className={`${styles["navbar-links"]} ${styles["large-screen"]}`}
+      style={{ marginLeft: "auto" }}
+    >
+      <a href="#link1">Food Library</a>
+      <a href="#link2">About</a>
+    </div>
+  );
+}
+
+export default Menu;
+export { HamButtom, MenuLinks };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import style from './NavBar.module.css'; // Assuming a CSS file for styling
+// import "./NavBar.css"; // Assuming a CSS file for styling
+import Menu, {HamButtom, MenuLinks} from './Menu.jsx'; // Importing the Menu component
+
 
 function NavBar() {
-    return (
-    <div style={{
-        borderBottom: '3px solid black',
-        height: '78px',
-        backgroundColor: '#6A3FFF',
-    }}>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    </div>
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <nav className={style.NavBar} style={{
+            borderBottom: '3px solid black',
+            height: '78px',
+            backgroundColor: '#6A3FFF',
+        }}>
+            <div className={style['navbar-container']}>
+                <Menu />
+                <HamButtom toggleMenu={toggleMenu} />
+            </div>
+            {isMenuOpen && (
+                <MenuLinks />
+                // <div className={style['dropdown-menu small-screen']}>
+                //     <a href="#link1">Food Library</a>
+                //     <a href="#link2">About</a>
+                // </div>
+            )}
+        </nav>
     );
-};
+}
 
 export default NavBar;
